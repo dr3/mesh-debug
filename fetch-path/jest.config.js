2d73b6f091f20866a251e876af3cf4c79d4b6929
Drew McMillan <drew@mcmill.co.uk>
2023-04-26 22:51:07 +0100

    Remove fetch file example, not a bug

--- fetch-path/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/tests/e2e/', '<rootDir>/tests/smoke/'],
-  rootDir: 'src',
-};
--- fetch-path/src/utils/fetch.ts
+++
@@ -1,6 +0,0 @@
-import { MeshContext } from "@graphql-mesh/runtime"
-
-export default(url: string, init: RequestInit, context: MeshContext) => {
-    console.log('CUSTOM FETCH', url);
-    return fetch(url, init);
-}
