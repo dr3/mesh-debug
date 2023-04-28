09718e645f8236dd2fbdd62e1de84f19d73d6fac
Drew McMillan <drew.mcmillan@thetrainline.com>
2023-04-28 12:51:52 +0100

    🔧 Continue to implement and debug

--- bare-enum-sending/src/server.ts
+++ bare-enum-sending/src/server.ts
@@ -1,38 +1,32 @@
 import fastify from "fastify";
 import { createBuiltMeshHTTPHandler } from "./.mesh/index";
 
-const getServer = async () => { 
-  const app = fastify()
-  
-  const meshHttp = createBuiltMeshHTTPHandler()
-  
-  app.route({
-    url: '/graphql',
-    method: ['GET', 'POST', 'OPTIONS'],
-    async handler(req, reply) {
-      const response = await meshHttp.handleNodeRequest(req, {
-        req,
-        reply,
-      });
-      response.headers.forEach((value: any, key: any) => {
-        reply.header(key, value);
-      });
+export const app = fastify()
 
-      reply.status(response.status);
+const meshHttp = createBuiltMeshHTTPHandler()
 
-      const reader = response.body!.getReader();
+app.route({
+  url: '/graphql',
+  method: ['GET', 'POST', 'OPTIONS'],
+  async handler(req, reply) {
+    const response = await meshHttp.handleNodeRequest(req, {
+      req,
+      reply,
+    });
+    response.headers.forEach((value: any, key: any) => {
+      reply.header(key, value);
+    });
 
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done) break;
-        reply.send(value);
-      }
+    reply.status(response.status);
 
-      return reply;
+    const reader = response.body!.getReader();
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      reply.send(value);
     }
-  })
 
-  return app;
-};
-
-export default getServer;
+    return reply;
+  }
+});
--- naming-convention-originalName/src/server.test.ts
+++ naming-convention-originalName/src/server.test.ts
@@ -1,5 +1,6 @@
-import getServer from "./server";
-import getUpstreamServer from "./upstream";
+import { fetch } from '@whatwg-node/fetch';
+import { app as server } from "./server";
+import { server as upstream } from "./upstream";
 
 const petQuery = `
 query myQuery(
@@ -25,36 +26,37 @@ query myQuery(
 `;
 
 describe("server", () => {
-  const upsteamApp = getUpstreamServer();
-
   beforeAll(async () => {
-    await upsteamApp.listen({ port: 8801 });
+    await server.listen({
+      port: 4000,
+    });
+    await upstream.listen({
+      port: 4001,
+    });
   });
 
   afterAll(async () => {
-    upsteamApp.close();
+    await server.close();
+    await upstream.close();
   });
 
   it("should return data", async () => {
-    const app = await getServer();
-
-    const response = await app.inject({
-      method: "POST",
-      url: "/graphql",
+    const response = await fetch('http://localhost:4000/graphql', {
+      method: 'POST',
       headers: {
-        accept: "application/json",
-        "content-type": "application/json",
+        'Content-Type': 'application/json',
       },
-      payload: JSON.stringify({
+      body: JSON.stringify({
         query: petQuery,
         variables: {
           petId: "pet200",
-        },
+        }
       }),
     });
 
-    // expect(response.statusCode).toEqual(200);
-    expect(JSON.parse(response.body)).toEqual({
+    const resJson = await response.json();
+    
+    expect(resJson).toEqual({
       data: {
         petByPetId: {
           id: "pet200",
